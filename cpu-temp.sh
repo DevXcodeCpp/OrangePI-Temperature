0a486cf3eb69305a98dc7a99292bca89d2839dee
DevXcode <148647264+DevXcodeCpp@users.noreply.github.com>
2026-04-26 21:11:43 +0300

    Translate CPU temperature monitor output to English

--- cpu-temp.sh
+++ cpu-temp.sh
@@ -6,6 +6,6 @@ while true
 do
     raw_temp=$(cat /sys/class/thermal/thermal_zone0/temp)
     temp=$((raw_temp / 1000))
-    printf "\r\e[32m[ MONITOR ]\e[0m Время: %s | Температура CPU: \e[31m%s°C\e[0m   " "$(date +%H:%M:%S)" "$temp"
+    printf "\r\e[32m[ MONITOR ]\e[0m Time: %s | Temperature CPU: \e[31m%s°C\e[0m   " "$(date +%H:%M:%S)" "$temp"
     sleep 1
 done
